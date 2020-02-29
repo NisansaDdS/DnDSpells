91da2c065024331221b1eddf00e58ee474017796
Nisansa Dilushan de Silva <NisansaDdS@cse.mrt.ac.lk>
2020-02-28 23:34:38 -0800

    Adding Bootstrap and images.

--- gen.js
+++ gen.js
@@ -1,17 +1,65 @@
 var template = `
 {{#spells}}
-<div class="spell">
-  <h3>{{name}}</h3>
-  <div class="type">{{type}}</div>
-  <b>Casting Time:</b> {{casting_time}}<br/>
-  <b>Range:</b> {{range}}<br/>
-  <b>Components:</b> {{component_desc}}{{#material_cost}} ({{material_desc}}){{/material_cost}}<br/>
-  <b>Duration:</b> {{duration}}<br/>
+<div class="spell card pmd-card bg-light">
+  
+  
+  
+  <div class="card-header">
+  
+  
+  <h3>
+  
+  {{#school}}
+  <img class="school" src="{{{school}}}.png">
+  {{/school}}
+  
+  {{name}}
+  
+  </h3>
+   
+  
+  {{#concentration}}
+  <img class="badges"  src="https://www.dndbeyond.com/content/1-0-842-0/skins/waterdeep/images/icons/core_mechanics/concentration.svg">
+  {{/concentration}}
+  
+  
+  
+  
+  </div>
+  <div class="card-body">
+  <table width="100%;">
+	<tr>
+		<td colspan="2"><div class="type">{{type}}</div></td>
+		<td><b>Casting Time:</b>{{casting_time}}</td>
+	</tr>
+	<tr>
+		<td width="30%;"><b>Range:</b><br>{{range}}</td>
+		<td width="20%;"><b>Components:</b><br>{{component_desc}}</td>
+		<td width="50%;"><b>Duration:</b><br>{{duration}}</td>
+	</tr>
+	{{#material_cost}}
+	<tr>
+		<td colspan="3">{{material_desc}}</td>	
+	</tr>
+	{{/material_cost}}
+	
+  </table>
+  <hr>
+  
   {{{desc}}}
-  {{#higher_level}}
-  <p><b>At higher levels:</b></p>
-  {{{higher_level}}}
+  
+  {{#higher_level}}  
+  <div class="card bg-info">
+  <div class="card-header inner-headder"><b>At higher levels:</b></div>
+  <div class="card-body inner-card">
+  {{{higher_level}}}   
+  </div>
+  </div>  
   {{/higher_level}}
+  
+  
+</div>
+
 </div>
 {{/spells}}
 `;
@@ -30,13 +78,13 @@ spellbook.sort(function(a, b) {
 
 for (let sp of spellbook) {
   if (sp.level === 0)
-    sp.type = `${sp.school} cantrip`
+    sp.type = `${sp.school} Cantrip`
   else
-    sp.type = `${sp.level_desc} ${sp.school.toLowerCase()}`
+    sp.type = `${sp.level_desc} ${sp.school}`
 }
 
 window.onload = function () {
-  document.getElementById('spells').innerHTML = Mustache.render(template, {spells: spellbook});
+  document.getElementById('spells').innerHTML ="<div class='card-columns'>"+Mustache.render(template, {spells: spellbook})+"</div>";
 }
 
 var closeHeader = function() {
